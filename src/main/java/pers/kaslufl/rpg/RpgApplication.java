524e41e8949a9d386af67273c1a30dad6374fd23
kaslufl <leallucasfonseca@gmail.com>
2021-09-28 15:17:46 -0300

    config: add project configurations

---
+++ src/main/java/pers/kaslufl/rpg/RpgApplication.java
@@ -0,0 +1,13 @@
+package pers.kaslufl.rpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpgApplication.class, args);
+    }
+
+}
